4401eb71847964288c4cc0611f783eb04781feef
tinlove <tinlove@gmail.com>
2017-07-23 15:24:09 +0900

    .

--- .shutdown.bat
+++ .shutdown.bat
@@ -1,2 +1,3 @@
 @echo off
-for /f "tokens=5" %%p in ('netstat -ano ^| find "LISTENING" ^| find "3003"') do taskkill /F /PID %%p || exit true
+for /f "tokens=5" %%p in ('netstat -ano ^| find "LISTENING" ^| find "3003"') do taskkill /F /PID %%p
+exit /b 0
