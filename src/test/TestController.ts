f3ee2cc1bf02e6518145317d575726373b593912
tinlove <tinlove@gmail.com>
2017-07-23 15:50:43 +0900

    .

--- src/test/TestController.ts
+++ src/test/TestController.ts
@@ -24,7 +24,7 @@ export class TestController {
         console.log("Get");
         console.log("queryParam:", queryParam);
 
-        res.send("Get");
+        res.send("GetTest12345");
     }
 
     @Post('/test')
