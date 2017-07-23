4319cb7d3d37bc196cfd192712d3469e725afd61
tinlove <tinlove@gmail.com>
2017-07-23 15:10:24 +0900

    .

--- .shutdown.bat
+++ .shutdown.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f "tokens=5" %%p in ('netstat -ano ^| find "LISTENING" ^| find "3003"') do taskkill /F /PID %%p 2>> error.txt
+for /f "tokens=5" %%p in ('netstat -ano ^| find "LISTENING" ^| find "3003"') do taskkill /F /PID %%p
